cbad48b767fa889c141e88aa95a06b59f0ea927e
techEmprez <choiceosobor@gmail.com>
2022-10-21 08:53:14 +0100

    create species table

--- queries.sql
+++ queries.sql
@@ -18,3 +18,36 @@ SELECT AVG(weight_kg) FROM animals;
 SELECT neutered, SUM(escape_attempts) FROM animals GROUP BY neutered;
 SELECT species, MIN(weight_kg), MAX(weight_kg) FROM animals GROUP BY species;
 SELECT species, AVG(escape_attempts) FROM animals WHERE date_of_birth BETWEEN '1990-01-01' AND '2000-12-31' GROUP BY species;
+
+
+-- QUERY MULTIPLE TABLES
+
+-- create a new table for owners
+CREATE TABLE owners(
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+  full_name VARCHAR(250) NOT NULL,
+  age INT NOT NULL,
+  PRIMARY KEY(id)
+);
+
+-- create a new table for species
+CREATE TABLE species (
+  id INT NOT NULL GENERATED ALWAYS AS IDENTITY,
+  name VARCHAR(250) NOT NULL,
+  PRIMARY KEY(id)
+);
+
+
+-- modify animals table 
+
+-- remove the species column
+ALTER TABLE animals DROP COLUMN species;
+
+-- add a new column species_id to the animal table
+ALTER TABLE animals ADD COLUMN species_id INT;
+
+ALTER TABLE animals ADD CONSTRAINT fk_animals_species FOREIGN KEY (species_id) REFERENCES species(id);
+
+-- add a new column owner_id to the animal table
+ALTER TABLE animals ADD COLUMN owner_id INT;
+ALTER TABLE animals ADD CONSTRAINT fk_animals_owners FOREIGN KEY (owner_id) REFERENCES owners(id);
